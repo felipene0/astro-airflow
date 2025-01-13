7deff5000791c873d3c796b7cc85bb205ca66df4
felipene0 <felipeepilefcv12@gmail.com>
2025-01-13 17:57:33 -0300

    Adding new components and uploading parquet file to GCS

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,2 @@
 FROM quay.io/astronomer/astro-runtime:12.6.0
+COPY .env /usr/local/airflow/.env
--- dags/astro-nyc-gcp-dtm.py
+++ dags/astro-nyc-gcp-dtm.py
@@ -11,24 +11,24 @@
 
 """
 
-# The code snippet bellow is reponsible for fetching data from the NYC Open data API using Dynamic Task Mapping for two different boroughs, and then storing in GCP.
+# The code snippet bellow is reponsible for fetching data from the NYC Open data API using Dynamic Task Mapping for two different boroughs, and then storing in GCS.
 
+import os
 import requests
 import logging
 import pandas as pd
 from datetime import datetime, timedelta
-from airflow.decorators import dag, task
-from airflow.utils.task_group import TaskGroup
-from airflow.operators.python import get_current_context
+from airflow.decorators import dag, task, task_group
 from dateutil.relativedelta import relativedelta
-
+from google.cloud import storage
 
 API = "https://data.cityofnewyork.us/resource/h9gi-nx95.json?$limit={limit}&$offset={offset}&$where=crash_date>='{date_filter}'&borough={borough}"
 BOROUGH_LIST = ['BROOKLYN', 'QUEENS']
 DATE_FILTER = (datetime.now() - relativedelta(weeks=10)).strftime("%Y-%m-%d")
 LIMIT = 50000
+GCS_BUCKET_NAME = os.getenv('GCS_BUCKET_NAME')
+GCS_OUTPUT_FOLDER = os.getenv('GCS_OUTPUT_FOLDER')
 
-# Configure Logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
 
 @dag(
@@ -39,40 +39,75 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
 )
 def main():
 
-    @task(map_index_template="{{ borough }}")
-    # @task
-    def fetch_data(borough: str):
-        offset = 0
-        all_data = []
-
-        # Use borough name in the task name
-        # context = get_current_context()
-        # context["borough"] = borough
-
-        try:
-            while True:
-                    api_url = API.format(limit=LIMIT, offset=offset, date_filter=DATE_FILTER, borough=borough)
-                    # response = requests.get(f"{API}?$limit={LIMIT}&$offset={offset}&$where=crash_date >= '{DATE_FILTER}'&$where=borough = '{}", timeout=20)
-                    response = requests.get(api_url, timeout=20)
-                    response.raise_for_status()
-
-                    data = response.json()
-
-                    logging.info(f"Fetched {len(data)} rows starting from offset {offset}")
-                    all_data.extend(data)
-
-                    # Break the loop if no more data is fetched
-                    if len(data) < LIMIT:
-                        break
-
-                    offset += LIMIT
-        except requests.exceptions.RequestException as e:
-            logging.error(f"Failed to fetch data for {borough}: {e}")
-            raise
+    @task_group(group_id="data_processing")
+    def data_processing():
         
-        return all_data
-    
-    # Dynamically map the fetch_data task over the list of boroughs
-    fetch_data.partial().expand(borough=BOROUGH_LIST)
+        @task(map_index_template="{{ borough }}")
+        def fetch_data_dtm(borough: str):
+            offset = 0
+            all_data = []
+            
+            try:
+                while True:
+                        api_url = API.format(limit=LIMIT, offset=offset, date_filter=DATE_FILTER, borough=borough)
+                        response = requests.get(api_url, timeout=20)
+                        response.raise_for_status()
+
+                        data = response.json()
+
+                        logging.info(f"Fetched {len(data)} rows starting from offset {offset}")
+                        all_data.extend(data)
+
+                        # Break the loop if no more data is fetched
+                        if len(data) < 1:
+                            break
+
+                        offset += LIMIT
+            except requests.exceptions.RequestException as e:
+                logging.error(f"Failed to fetch data for {borough}: {e}")
+                raise
+
+            df = pd.DataFrame(all_data)
+            local_file = f"/tmp/{borough}_crash_data.parquet"
+            df.to_parquet(local_file, engine="pyarrow", index=False)
+            logging.info(f"Saved {borough} into a DataFrame with shape {df.shape} in {local_file}")
+            
+            return local_file
+
+        @task
+        def combine_files(file_path: list) -> pd.DataFrame:
+            combined_df = pd.DataFrame()
+
+            for file in file_path:
+                df = pd.read_parquet(file)
+                combined_df = pd.concat([combined_df, df], ignore_index=True)
+
+            combined_local_file = "/tmp/crash_data.parquet"
+            combined_df.to_parquet(combined_local_file, engine="pyarrow", index=False)
+            logging.info(f"Saved combined Dataframe to {combined_local_file}")
+
+            return combined_local_file
+        
+        #Group Depedencies
+        file_paths = fetch_data_dtm.expand(borough=BOROUGH_LIST)
+        combined_data = combine_files(file_paths)
+
+        return combined_data
+            
+    @task
+    def upload_gcs(file_path: list):
+        client = storage.Client()
+
+        bucket = client.bucket(GCS_BUCKET_NAME)
+        blob = bucket.blob(f"{GCS_OUTPUT_FOLDER}crash_data.parquet")
+        blob.upload_from_filename(file_path)
+
+        logging.info(f"Uploaded {file_path} to GCS at {GCS_OUTPUT_FOLDER} as {blob}")
+        
+        # Clean up local file
+        os.remove(file_path)
+
+    final_data = data_processing()
+    upload_gcs(final_data)
 
 main()
